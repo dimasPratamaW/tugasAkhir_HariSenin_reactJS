cfcbfc3f02f9d956927b1ac87752ceef7b824c72
Rizkyth <rizkyth002@gmail.com>
2024-05-04 17:06:30 +0700

    fixed dependencies

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,10 @@
-import React from 'react'
-import RootLayouts from '../../layouts/RootLayouts'
-import Gambar from '../../components/molecules/Gambar'
-import MainSection from '../../components/MainSection'
+import RootLayouts from "../../layouts/RootLayouts";
+import MainSection from "../../components/MainSection";
 
 export default function HomePage() {
   return (
     <RootLayouts>
-        <MainSection />
+      <MainSection />
     </RootLayouts>
-
-  )
+  );
 }
