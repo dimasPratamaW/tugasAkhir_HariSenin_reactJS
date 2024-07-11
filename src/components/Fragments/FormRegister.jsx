b9dbbf0ce9eb5feb8300a5431fab209a7adf8243
RandaAlfizah <randaalfiizh@gmail.com>
2024-07-11 16:15:51 +0700

    udah berhasil konekin login ama register ke Db

--- src/components/Fragments/FormRegister.jsx
+++ src/components/Fragments/FormRegister.jsx
@@ -1,15 +1,26 @@
 import Button from "../Elements/Button/Index.jsx";
 import InputForm from "../Elements/Input/Index.jsx";
+import api from "../../api.js"; // import the api instance
 
 const FormRegister = () => {
-  const handleRegister = (event)=> {
+  const handleRegister = async (event)=> {
+    event.preventDefault();
+    const email= event.target.email.value;
+    const password = event.target.password.value;
+    const name = event.target.name.value;
+
+    try {
+      const response = await api.post("/register", { email, password, name})
+      window.location.href = "/login";
+    } catch (error) {
+      
+    }
     localStorage.setItem("email", event.target.email.value);
-    localStorage.setItem("fullname", event.target.fullname.value);
+    localStorage.setItem("name", event.target.name.value);
     localStorage.setItem("password", event.target.password.value);
     // console.log(event.target.email.value);
     // console.log(event.target.password.value);
-    event.preventDefault();
-    window.location.href = "/login";
+
   };
     return (
         <form onSubmit={handleRegister}>
@@ -23,7 +34,7 @@ const FormRegister = () => {
             label="Nama Anda" 
             type="text" 
             placeholder="Misalnya John Doe" 
-            name="fullname"
+            name="name"
             />   
             <InputForm.InputPassword
             label="Password" 
